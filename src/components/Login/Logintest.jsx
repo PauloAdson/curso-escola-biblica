bb900aa37f8b651086cd9d97c6ad596c54bc4680
Paulo Adson <paulo.alves200581@gmail.com>
2024-09-10 15:30:36 -0300

    estilizando a página de login

--- src/components/Login/Logintest.jsx
+++ src/components/Login/Logintest.jsx
@@ -6,38 +6,47 @@ export class Logintest extends React.Component {
         return (
             <>
                 <main className="bg-login-cadastro">
-                    <div className="container-form-login ">
-                        <div className="title-form">
-                            <h1>Faça o seu login</h1>
+                    <div className="container-form-login">
+                        <div className="container-title">
+                            <h1 className="title-form">Faça o seu login</h1>
                             <div className="title-underline"></div>
                         </div>
 
                         <form className="login-form" action="">
                             <label htmlFor="email">Email*</label>
-                            <input type="email"
+                            <input
+                                className="input-text"
+                                type="email"
                                 name="email"
                                 id="email"
                                 required />
 
                             <label htmlFor="password">Senha*</label>
                             <input
+                                className="input-text espaco"
                                 type="password"
                                 name="password"
                                 id="password"
                                 required />
+                            <div>
+                                <input
+                                    className="espaco"
+                                    type="checkbox"
+                                    name="termos"
+                                    id="termos" />
 
-                            <input type="checkbox"
-                                name="termos"
-                                id="termos" />
-                            <label htmlFor="">Lembrar-me</label>
+                                <label htmlFor="termos">Lembrar-me</label>
+                            </div>
 
                             <button
+                                className="espaco"
                                 type="submit"
                             >Entrar</button>
                         </form>
-                        <p>Não possui uma conta? <a href="/cadastro">Criar aqui</a></p>
 
-                        <a href="/">Voltar ao ínicio</a>
+                        <p className="espaco weight">Não possui uma conta? <a href="/cadastro" className="cor-destaque">Criar aqui</a></p>
+
+                        <a href="/" className="cor-destaque weight">Voltar ao ínicio</a>
 
                     </div>
                 </main>
